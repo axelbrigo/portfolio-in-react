1805d20a2d43aaaceb5485aa74ed6073e89b6940
Axel Brigo <developer@axelbrigo.com>
2022-04-14 17:56:16 +0200

    actualizacion del cv

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -3,27 +3,27 @@ import './topbar.css'
 
 
 
-export default function TopBar({menuOpen, setMenuOpen}) {
+export default function TopBar({ menuOpen, setMenuOpen }) {
     return (
         <div className={'topbar ' + (menuOpen && "active")}>
             <div className='wrapper'>
                 <div className='left'>
                     <a href='#intro' className='logo'>AXELBRIGO.COM</a>
                     <div className="itemContainer">
-                    <i className="fas fa-phone-square icon"></i>
-                        <span>+34 604 12 07 23</span>
+                        <i className="fas fa-phone-square icon"></i>
+                        <span>+45 31886168</span>
                     </div>
                     <div className="itemContainer">
-                    <i class="fas fa-envelope icon"></i>
-                        <span>developer@axelbrigo.com</span>
+                        <i class="fas fa-envelope icon"></i>
+                        <span>theaxelbrigo@gmail.com</span>
                     </div>
                 </div>
                 <div className='righ'>
-                     <div className="hamburguer" onClick={() => setMenuOpen(!menuOpen)}>
-                         <span className='line1'></span>
-                         <span className='line2'></span>
-                         <span className='line3'></span>
-                     </div>
+                    <div className="hamburguer" onClick={() => setMenuOpen(!menuOpen)}>
+                        <span className='line1'></span>
+                        <span className='line2'></span>
+                        <span className='line3'></span>
+                    </div>
                 </div>
             </div>
         </div>
